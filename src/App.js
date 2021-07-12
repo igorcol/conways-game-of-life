f295a954a9c6ff9abda81828c3c859d3b5515120
Igor Colombini <igor.colombini@gmail.com>
2021-07-11 22:20:02 -0300

    HUE and DeathMode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useRef, useCallback } from 'react'
 import produce from 'immer'
+import { SliderPicker } from 'react-color';
 
 import './App.css';
 
 const numRows = 70;
 const numCols = 70;
-const activeNodeColor = "black"
 const delay = 100
 
 // Node neighbors index
@@ -30,6 +30,10 @@ function generateEmptyGrid() {
 
 function App() {
   const [running, setRunning] = useState(false)
+  const [color, setColor] = useState("#111")
+  let [counter, setCounter] = useState(0)
+  const [deathMode, setDeathMode] = useState(false)
+  
 
   const [grid, setGrid] = useState(() => {
     return generateEmptyGrid()
@@ -49,10 +53,11 @@ function App() {
       */
       setGrid(g => {
         return produce(g, gridCopy => {
+          // For each cell 
           for (let i = 0; i < numRows; i++) {
             for (let k = 0; k < numCols; k++) {
-              // For each cell 
               let neighbors = 0;
+
               // Get neighbors
               operations.forEach(([x, y]) => {
                 const newI = i + x
@@ -63,6 +68,7 @@ function App() {
                 }
               })
 
+              
               // *1 
               if (neighbors < 2 || neighbors > 3) {
                 gridCopy[i][k] = 0;
@@ -71,7 +77,8 @@ function App() {
               else if (g[i][k] === 0 && neighbors === 3) {
                 gridCopy[i][k] = 1;
               }
-
+              
+            
             }
           }
         })
@@ -79,26 +86,67 @@ function App() {
     setTimeout(runSimulation, delay)
   }, [])
 
+  function handleStartSimulation() {
+    setRunning(!running);
+    if(!running) {
+      runningRef.current = true
+      runSimulation()
+    }
+  }
+  
+  // Generates new grid with new live cell
+  function handleNodeClick(i,k) {
+    const newGrid = produce(grid, gridCopy => {
+    gridCopy[i][k] = !grid[i][k] })
+    setGrid(newGrid) 
+  }
 
+  // Generates random live cell pattern
+  function handleRandomizeGrid() {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => Math.random() > 0.6 ? 1 : 0))
+    }
+    setGrid(rows)
+  }
+
+  // Death Mode EasterEgg
+  function handleDeathMode() {
+      if (counter < 4) {
+        setCounter(counter + 1) 
+        setDeathMode(false)
+      }
+      else if (counter === 4){
+        setDeathMode(true)
+        handleRandomizeGrid()
+        handleStartSimulation()
+        setColor("#EE3633")
+        setCounter(-1) 
+      }
+  }
+
+  // RENDER
   return (
     <div className="page">
-
-      <div className="title">
-        <h1>Conway's Game Of Life</h1>
+      <div className="title" 
+        onClick={() => handleDeathMode()}
+        style={{
+          color: deathMode ? "#EE3633" : "#1860B6"
+      }}>
+        <h1>
+          {deathMode ? `Conway's Game Of Death` : `Conway's Game Of Life`}
+        </h1>
       </div>
 
       <div className="btns">
         <a  className="startBtn" 
-        style={{
-          backgroundColor: running ? "#EE3633" : "#1860B6"
-        }} 
-        onClick={() => {
-          setRunning(!running);
-          if(!running) {
-            runningRef.current = true
-            runSimulation()
-          }
-        }}> {running ? "STOP" : "START"} </a>
+          style={{
+            backgroundColor: running ? "#EE3633" : "#1860B6",
+            color: "#fff"
+          }} 
+          onClick={handleStartSimulation}> 
+        {running ? "STOP" : "START"} 
+        </a>
 
         <a  className="clearBtn" onClick={() => {
           setGrid(generateEmptyGrid())
@@ -106,18 +154,24 @@ function App() {
           if(running) {
             runningRef.current = false
           }
-        }}>CLEAR</a>
+        }}> 
+          CLEAR
+        </a>
 
-        <a  className="randomizeBtn" onClick={() => {
-          const rows = [];
-          for (let i = 0; i < numRows; i++) {
-            rows.push(Array.from(Array(numCols), () => Math.random() > 0.6 ? 1 : 0))
-          }
-          setGrid(rows)
-        }}>RANDOMIZE</a>
+        <a  className="randomizeBtn" onClick={handleRandomizeGrid}>
+          RANDOMIZE
+        </a>
+
+        <SliderPicker className="colorPicker"
+          color={color}
+          onChangeComplete={(color) => {
+            setColor(color.hex)
+          }}
+        />
 
         
       </div>
+    
 
       <div className="grid" style={{
           display: 'grid',
@@ -127,21 +181,20 @@ function App() {
           rows.map((col, k) => 
             <div className="node"
             key={`${i}-${k}`}
-            onClick={() => {
-              const newGrid = produce(grid, gridCopy => {
-                gridCopy[i][k] = !grid[i][k]
-              })
-              setGrid(newGrid)
-            }}
+            onClick={() => handleNodeClick(i,k)}
             style={{
               width: 20,
               height: 20,
               border: 'solid 1px grey',
-              backgroundColor: grid[i][k] ? activeNodeColor : undefined
+              backgroundColor: grid[i][k] ? color : undefined
             }} 
             />
           ))}
       </div>
+
+      <footer>  
+          <a href="https://github.com/igorcol/conways-game-of-life">github.com/igorcol</a>
+      </footer>
 
     </div>
   );
